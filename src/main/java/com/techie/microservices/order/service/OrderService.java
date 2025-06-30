df651d013a0e9dc9953711fd46c1b7a3bf1f746a
kenRp01 <yqxxnaxr1109@gmail.com>
2025-07-01 02:54:45 +0900

    spring.cloud,wiremockを追加

--- src/main/java/com/techie/microservices/order/service/OrderService.java
+++ src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.order.service;
 
+import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.dto.OrderRequest;
 import com.techie.microservices.order.model.Order;
 import com.techie.microservices.order.repositry.OrderRepository;
@@ -13,7 +14,13 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     public void placeOrder(OrderRequest orderRequest) {
+        boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (!isInStock) {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setPrice(orderRequest.price());
--- src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -1,13 +1,63 @@
 package com.techie.microservices.order;
 
+import com.techie.microservices.order.stubs.InventoryClientStub;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.shaded.org.hamcrest.Matchers;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+import static org.assertj.core.groups.FieldsOrPropertiesExtractor.extract;
+import static org.testcontainers.shaded.org.hamcrest.MatcherAssert.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mysqlContainer = new MySQLContainer("mysql:8.0.32");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mysqlContainer.start();
+	}
+
+	@Test
+	void shouldSubmitorder() {
+		String submitOrderJson = """
+     			{
+					"skuCode": "iphone_15",
+					"price": 1000,
+					"quantity": 1
+				}
+				""";
+
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
+
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+		assertThat(responseBodyString, Matchers.is("Order Placed Success"));
+	}
 	@Test
 	void contextLoads() {
 	}
